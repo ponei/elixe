182ebbfa4627d518c1426dfe350c6222a6ec2d1e
ponei <poneialt@gmail.com>
2020-12-05 19:51:30 -0300

    Create OnPushOutBlocksEvent.java
    evento lol!

---
+++ events/OnPushOutBlocksEvent.java
@@ -0,0 +1,9 @@
+package elixe.events;
+
+import me.zero.alpine.event.type.Cancellable;
+
+//pushOutOfBlocks(double, double, double) : boolean - net.minecraft.client.entity.EntityPlayerSP
+//L:463
+public class OnPushOutBlocksEvent extends Cancellable {
+
+}
