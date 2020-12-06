d5551cb76ca1239372d230a13b1da71f57f42d28
ponei <poneialt@gmail.com>
2020-12-06 18:46:54 -0300

    velocity tem mais checks

--- modules/combat/Velocity.java
+++ modules/combat/Velocity.java
@@ -4,10 +4,12 @@ import elixe.events.OnPacketReceiveEvent;
 import elixe.events.OnRenderNameEvent;
 import elixe.modules.Module;
 import elixe.modules.ModuleCategory;
+import elixe.modules.option.ModuleBoolean;
 import elixe.modules.option.ModuleFloat;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import net.minecraft.network.play.server.S12PacketEntityVelocity;
+import net.minecraft.util.MovingObjectPosition.MovingObjectType;
 
 public class Velocity extends Module {
 	public Velocity() {
@@ -15,6 +17,11 @@ public class Velocity extends Module {
 
 		moduleOptions.add(horizontalMultiplierOption);
 		moduleOptions.add(verticalMultiplierOption);
+
+		moduleOptions.add(needSprintOption);
+		moduleOptions.add(needAttackButtonOption);
+		moduleOptions.add(needWeaponOption);
+		moduleOptions.add(waterCheckOption);
 	}
 
 	float horizontalMultiplier = 0.9f;
@@ -31,18 +38,77 @@ public class Velocity extends Module {
 		}
 	};
 
+	boolean needSprint = false;
+	ModuleBoolean needSprintOption = new ModuleBoolean("require sprint", false) {
+		public void valueChanged() {
+			needSprint = (boolean) this.getValue();
+		}
+	};
+
+	boolean needAttackButton = false;
+	ModuleBoolean needAttackButtonOption = new ModuleBoolean("require attack button", false) {
+		public void valueChanged() {
+			needAttackButton = (boolean) this.getValue();
+		}
+	};
+
+	boolean needWeapon = false;
+	ModuleBoolean needWeaponOption = new ModuleBoolean("require weapon", false) {
+		public void valueChanged() {
+			needWeapon = (boolean) this.getValue();
+		}
+	};
+
+	boolean waterCheck = false;
+	ModuleBoolean waterCheckOption = new ModuleBoolean("water check", false) {
+		public void valueChanged() {
+			waterCheck = (boolean) this.getValue();
+		}
+	};
+
 	@EventHandler
 	private Listener<OnPacketReceiveEvent> onPacketReceiveEvent = new Listener<>(e -> {
 		if (e.getPacket() instanceof S12PacketEntityVelocity) {
 			S12PacketEntityVelocity vel = (S12PacketEntityVelocity) e.getPacket();
 			if (vel.getEntityID() == mc.thePlayer.getEntityId()) {
-				vel.setMotionX((int) (vel.getMotionX() * horizontalMultiplier));
-				vel.setMotionZ((int) (vel.getMotionZ() * horizontalMultiplier));
+				if (shouldModifyVelocity()) {
+					vel.setMotionX((int) (vel.getMotionX() * horizontalMultiplier));
+					vel.setMotionZ((int) (vel.getMotionZ() * horizontalMultiplier));
 
-				vel.setMotionY((int) (vel.getMotionY() * verticalMultiplier));
+					vel.setMotionY((int) (vel.getMotionY() * verticalMultiplier));
 
-				e.setPacket(vel);
+					e.setPacket(vel);
+				}
 			}
 		}
 	});
+
+	private boolean shouldModifyVelocity() {
+
+		if (needSprint) {
+			if (!conditionals.isSprinting()) {
+				return false;
+			}
+		}
+
+		if (needAttackButton) {
+			if (!conditionals.isHoldingAttack()) {
+				return false;
+			}
+		}
+
+		if (needWeapon) {
+			if (!conditionals.isHoldingWeapon()) {
+				return false;
+			}
+		}
+
+		if (waterCheck) {
+			if (conditionals.isInWater()) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
