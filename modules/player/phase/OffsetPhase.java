142965be1b8c8cb9833542ba932f3f93ff324fd6
ponei <poneialt@gmail.com>
2020-12-06 01:27:30 -0300

    offset phase

--- modules/player/phase/OffsetPhase.java
+++ modules/player/phase/OffsetPhase.java
@@ -1,32 +1,65 @@
 package elixe.modules.player.phase;
 
+import elixe.Elixe;
 import elixe.events.OnLivingUpdateEvent;
 import elixe.events.OnMoveEvent;
 import elixe.events.OnPacketSendEvent;
+import elixe.utils.player.RotationUtils;
+import net.minecraft.client.Minecraft;
 
 public class OffsetPhase implements IPhaseType {
 
+	Minecraft mc = Elixe.INSTANCE.mc;
+
+	boolean clip;
+	int step;
+
 	@Override
 	public void AddOptions() {
-		// TODO Auto-generated method stub
 		
 	}
 
+	double direction;
 	@Override
 	public void OnMove(OnMoveEvent event) {
-		// TODO Auto-generated method stub
-		
+		if (mc.thePlayer.isCollidedHorizontally) {
+			clip = true;
+		}
+		if (!clip)
+			return;
+
+		event.setX(0);
+		event.setZ(0);
+		step++;
+
+		switch (step) {
+		case 1:
+			direction = RotationUtils.getDirection(mc.thePlayer);
+			clipOffset(0.0625D);
+			break;
+		case 2:
+			clipOffset(1.5D);
+			break;
+		case 3:
+			step = 0;
+			direction = 0;
+			clip = false;
+			break;
+		}
+	}
+	
+	private void clipOffset(double offset) {
+		mc.thePlayer.setPosition(mc.thePlayer.posX + (-Math.sin(direction) * offset), mc.thePlayer.posY,
+				mc.thePlayer.posZ + (Math.cos(direction) * offset));
 	}
 
 	@Override
 	public void OnLivingUpdate(OnLivingUpdateEvent event) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void OnPacket(OnPacketSendEvent event) {
-		// TODO Auto-generated method stub
 		
 	}
 
