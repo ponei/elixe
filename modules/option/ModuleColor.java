08a08c20bc55987b531081052a9a7c9530a25342
ponei <poneialt@gmail.com>
2021-03-14 00:55:08 -0300

    transparencia no controle de cor

--- modules/option/ModuleColor.java
+++ modules/option/ModuleColor.java
@@ -10,7 +10,7 @@ public class ModuleColor extends AModuleOption {
 	public ModuleColor(String name, int r, int g, int b) {
 		super();
 		this.name = name;
-		RGBToInt(r, g, b);
+		RGBToInt(255, r, g, b);
 		valueChanged();
 	}
 
@@ -23,38 +23,39 @@ public class ModuleColor extends AModuleOption {
 	}
 
 	// https://stackoverflow.com/questions/4801366/convert-rgb-values-to-integer#4801397
-	private void RGBToInt(int r, int g, int b) {
-		this.color = ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | (b & 0x0ff);
+	private void RGBToInt(int a, int r, int g, int b) {
+		this.color = ((a & 0x0ff) << 24) | ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | (b & 0x0ff);
 	}
 	
-	private void GLRGBToInt(float r, float g, float b) {
-		this.color = (((int)(r * 255f) & 0x0ff) << 16) | (((int)(g  * 255f) & 0x0ff) << 8) | ((int)(b  * 255f) & 0x0ff);
+	private void GLRGBToInt(float a, float r, float g, float b) {
+		this.color = (((int)(a * 255f) & 0x0ff) << 24) | (((int)(r * 255f) & 0x0ff) << 16) | (((int)(g  * 255f) & 0x0ff) << 8) | ((int)(b  * 255f) & 0x0ff);
 	}
 
 	public float[] getGLRGB() {
+		float a = ((color >> 24) & 0x0ff) / 255f;
 		float r = ((color >> 16) & 0x0ff) / 255f;
 		float g = ((color >> 8) & 0x0ff) / 255f;
 		float b = ((color) & 0x0ff) / 255f;
-		return new float[] { r, g, b };
+		return new float[] { r, g, b, a };
 	}
 	
 	public int[] getRGB() {
+		int a = ((color >> 24) & 0x0ff);
 		int r = ((color >> 16) & 0x0ff);
 		int g = ((color >> 8) & 0x0ff);
 		int b = ((color) & 0x0ff);
-		return new int[] { r, g, b };
+		return new int[] { r, g, b, a };
 	}
 
-	public void setValueRGB(int r, int g, int b) {
-		RGBToInt(r, g, b);
+	public void setValueRGB(int r, int g, int b, int a) {
+		RGBToInt(a, r, g, b);
 		valueChanged();
 	}
 	
-	public void setValueGLRGB(float r, float g, float b) {
-		GLRGBToInt(r, g, b);
+	public void setValueGLRGB(float r, float g, float b, float a) {
+		GLRGBToInt(a, r, g, b);
 		valueChanged();
 	}
-
 
 
 	public Object getValue() {
