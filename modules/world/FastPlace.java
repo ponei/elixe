f5fdae3d3aef4739e9f1f3deb47cff235173397b
ponei <poneialt@gmail.com>
2021-04-06 22:49:07 -0300

    fastplace cps

--- modules/world/FastPlace.java
+++ modules/world/FastPlace.java
@@ -1,5 +1,7 @@
 package elixe.modules.world;
 
+import java.util.Random;
+
 import elixe.events.OnPlayerMoveStateEvent;
 import elixe.events.OnRightClickDelayTimerEvent;
 import elixe.modules.Module;
@@ -15,14 +17,49 @@ public class FastPlace extends Module {
 	public FastPlace() {
 		super("FastPlace", ModuleCategory.WORLD);
 		
-		moduleOptions.add(rightClickTimerOption);
+		moduleOptions.add(cpsMinOption);
+		moduleOptions.add(cpsMaxOption);
 		moduleOptions.add(needBlockOption);
 	} 
 	
-	int rightClickTimer;
-	ModuleInteger rightClickTimerOption = new ModuleInteger("right click timer", 3, 0, 10) {
+	int cpsMin;
+	ModuleInteger cpsMinOption = new ModuleInteger("cps min", 6, 0, 20) {
 		public void valueChanged() {
-			rightClickTimer = (int) this.getValue();
+			int newCps = (int) this.getValue();
+
+			if (cpsMaxOption != null) {
+				if (newCps > cpsMax) {
+					// fix
+					cpsMax = newCps;
+					if (cpsMaxOption.getButton() != null) {
+						cpsMaxOption.getButton().setValue(newCps);
+					}
+
+					cpsMaxOption.setValueSilent(newCps);
+				}
+			}
+
+			cpsMin = newCps;
+		}
+	};
+
+	int cpsMax;
+	ModuleInteger cpsMaxOption = new ModuleInteger("cps max", 10, 0, 20) {
+		public void valueChanged() {
+			int newCps = (int) this.getValue();
+
+			if (cpsMinOption != null) {
+				if (cpsMin > newCps) {
+					// fix
+					cpsMin = newCps;
+					if (cpsMinOption.getButton() != null) {
+						cpsMinOption.getButton().setValue(newCps);
+					}
+					cpsMinOption.setValueSilent(newCps);
+				}
+			}
+
+			cpsMax = newCps;
 		}
 	};
 	
@@ -33,6 +70,8 @@ public class FastPlace extends Module {
 		}
 	};
 	
+	Random r = new Random();
+	
 	@EventHandler
 	private Listener<OnRightClickDelayTimerEvent> onRightClickDelayTimerEvent = new Listener<>(e -> {
 		if (needBlock) {
@@ -40,6 +79,8 @@ public class FastPlace extends Module {
 				return;
 			}
 		}
-		e.setRightClickDelayTimer(rightClickTimer);
+		int cps = r.nextInt(cpsMax - cpsMin + 1)+cpsMin;
+		//System.out.println(20 / cps);
+		e.setRightClickDelayTimer(20 / cps);
 	});
 }
